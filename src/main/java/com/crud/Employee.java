0cbc980d08cb5ad1a24a5630ab80df38e21e99df
Sazid24Ali <shaikmohammedsazidali@gmail.com>
2023-09-27 14:36:10 +0530

    First Commit

---
+++ src/main/java/com/crud/Employee.java
@@ -0,0 +1,57 @@
+package com.crud;
+
+public class Employee {
+    private int eid;
+    private String ename;
+    private int age;
+    private int salary;
+
+    public Employee(int eid, String ename, int age, int salary) {
+        this.eid = eid;
+        this.ename = ename;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public Employee() {
+        // Empty COnstructor
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return this.eid + " : " + this.ename + " : " + this.age + " : " + this.salary;
+    }
+
+}
